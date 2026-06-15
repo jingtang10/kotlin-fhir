d03ce59e04e5d643259cea288bff28a354854ebb
Jing Tang <jingtang@google.com>
2026-06-15 18:10:25 +0100

    Fix copyright header warning (#99)
    * Fix copyright header warning
    
    * Update comment
    
    * Fix grammar mistake

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,13 +9,28 @@ plugins {
 
 configure<com.diffplug.gradle.spotless.SpotlessExtension> {
     ratchetFrom = "origin/main"
+
     kotlin {
         target("**/*.kt")
+        targetExclude("**/fhir-model-r*/**/*.kt")
         ktfmt().googleStyle()
         licenseHeaderFile(
             "license-header.txt",
         )
     }
+
+    // Due to the ratchetFrom setting, Spotless always tries to parse the existing copyright year
+    // before updating it to a range (e.g. 2025-2026). However, generated files do not have the
+    // copyright header initially. To avoid warnings, a statically resolved year is used.
+    val currentYear = java.time.LocalDate.now().year.toString()
+    val licenseHeaderTemplate = file("license-header.txt").readText()
+    val generatedLicenseHeader = licenseHeaderTemplate.replace("\$YEAR", currentYear)
+    format("generatedKotlin", com.diffplug.gradle.spotless.KotlinExtension::class.java) {
+        target("fhir-model-r4/src/**/*.kt", "fhir-model-r4b/src/**/*.kt", "fhir-model-r5/src/**/*.kt")
+        ktfmt().googleStyle()
+        licenseHeader(generatedLicenseHeader)
+    }
+
     flexmark {
         target("**/*.md")
         flexmark()
@@ -24,6 +39,9 @@ configure<com.diffplug.gradle.spotless.SpotlessExtension> {
 
 // Make sure codegen is run before formatting
 tasks.named("spotlessKotlin") {
+    dependsOn(":fhir-model-r4:codegen", ":fhir-model-r4b:codegen", ":fhir-model-r5:codegen")
+}
+tasks.named("spotlessGeneratedKotlin") {
     dependsOn(":fhir-model-r4:codegen", ":fhir-model-r4b:codegen", ":fhir-model-r5:codegen")
 }
 tasks.named("spotlessFlexmark") {
